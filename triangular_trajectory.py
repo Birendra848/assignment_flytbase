f0d42887a77e9735f5355b77e5d40c283c0a50e4
Birendra848 <75802334+Birendra848@users.noreply.github.com>
2022-06-11 21:26:13 +0530

    Update triangular_trajectory.py

--- triangular_trajectory.py
+++ triangular_trajectory.py
@@ -10,7 +10,7 @@ print '0. taking off till 10m'
 drone.take_off(10.0)
 print 'taking down 5 m'
 
-print ' move in a tfiangle trajectory of side length 10m at a height of 5m'
+print ' move in a triangular trajectory of side length 10m at a height of 5m'
 
 print ' 1. moving from Point A to B of triangle ABC'
 # h^2=p^2+b^2, here square root of sum of square of p=8m and b=6m
